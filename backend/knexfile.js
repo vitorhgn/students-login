126bad5b9c73ba3fd896a5afef6460d9b6cf0ebd
Vitor Hugo Gouveia Nunes <vitorhgn100@gmail.com>
2023-01-25 14:08:55 -0400

    Adicionando migrate e o knex

---
+++ backend/knexfile.js
@@ -0,0 +1,14 @@
+module.exports = {
+      test: {
+      client: "mysql",
+      connection: {
+      host: "162.241.203.130",
+      user: "jonath31_vitor_hugo_user",
+      password: "MySqlDatabase",
+      database: "jonath31_vitor_hugo_database",
+      },
+      migrations: {
+      directory: "migrations",
+      },
+   },
+};
