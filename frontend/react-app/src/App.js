dbd606f4fda6af44ce1cf38cfdfebb0dcfb0da15
Vitor Hugo Gouveia Nunes <vitorhgn100@gmail.com>
2023-01-28 18:28:25 -0400

    criando componente navbar e separando o css dele

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,15 +1,39 @@
 import './App.css';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello World!
-        </h1>
-      </header>
+<div className="main-container">
+<Navbar />
+    <section className="container">
+        <header className="main-header">
+            Consulta de Alunos
+        </header>
+        <div className="loader"></div>
+        <div className="content-page">
+            <div className="padding-left-right-20">
+                <div className="top-actions">
+                    <form id="formSearchStudent" className="form-search" action="">
+                        <input type="text" name="searchInput" id="searchInput"/>
+                        <button>Pesquisar</button>
+                    </form>
+                    <a className="btn btn-dark" href="studentsManager.html">Cadastrar Aluno</a>
+                </div>
+                <table id='studentList' class="table-list">
+                    <thead>
+                        <tr>
+                            <th>Registro Acadêmico</th>
+                            <th>Nome</th>
+                            <th>CPF</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
     </div>
   );
 }
-
 export default App;
---
+++ frontend/react-app/src/components/Navbar/index.jsx
@@ -0,0 +1,16 @@
+import './style.css';
+const Navbar = ()=>{
+    return (
+    <nav className="main-nav">
+        <header>
+            Módulo Academico
+        </header>
+        <ul className="nav-links">
+            <a className="nav-item" href="#">
+                <li>Alunos</li>
+            </a>
+        </ul>
+    </nav>
+);
+};
+export default Navbar;
